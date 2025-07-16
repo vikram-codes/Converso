f14b1dee661c050b629f3f665f0110b47f000275
vikram-codes <vikramjit.personal@gmail.com>
2025-07-16 03:57:15 -0400

    Creating CompanionsList & CTA on HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
@@ -34,7 +35,11 @@ const Page = () => {
         />
       </section>
       <section className="home-section">
-        <CompanionsList />
+        <CompanionsList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <Cta />
       </section>
     </main>
