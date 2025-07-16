1f6c8d149970204a4694fbccebc9a3cee3996d15
vikram-codes <vikramjit.personal@gmail.com>
2025-07-16 04:48:33 -0400

    Companion Form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,14 @@
+import CompanionForm from "@/components/CompanionForm";
+
 function NewCompanion() {
-  return <div>New Companion</div>;
+  return (
+    <main className="min-lg:w-1/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 }
 
 export default NewCompanion;
