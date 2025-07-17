70db1d36d8d46a419d5b3dcf79bd982253f109b9
vikram-codes <vikramjit.personal@gmail.com>
2025-07-16 21:02:59 -0400

    Implementing Supabase with Clerk Integration

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,12 @@
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function NewCompanion() {
+async function NewCompanion() {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
   return (
     <main className="min-lg:w-1/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { subjectsColors, /* voices */ } from "@/constants";
+import { subjectsColors /* voices */ } from "@/constants";
 // import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -41,7 +41,7 @@ export const getSubjectColor = (subject: string) => {
 //         {
 //           role: "system",
 //           content: `You are a highly knowledgeable tutor teaching a real-time voice session with a student. Your goal is to teach the student about the topic and subject.
-  
+
 //                     Tutor Guidelines:
 //                     Stick to the given topic - {{ topic }} and subject - {{ subject }} and teach the student about it.
 //                     Keep the conversation flowing smoothly while maintaining control.
